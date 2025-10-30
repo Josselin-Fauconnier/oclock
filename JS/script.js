4aa11b27f7b23d159be91cc6286314076eb886b5
Jossnnier <fauconnierjosselin@yahoo.fr>
2025-10-30 10:49:02 +0100

    index

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+
+function tick() {
+  
+  const now = new Date();
+  const hours = now.getHours() % 12;          
+  const minutes = now.getMinutes();           
+  const secondes = now.getSeconds();          
+
+  
+  const hourDeg = (hours * 30) + (minutes * 0.5);  
+
+  
+  const minutesDeg = (minutes * 6) + (secondes * 0.1);
+
+  
+  document.getElementById('hand-hour').style.transform =
+    `translate(-50%, 0) rotate(${hourDeg}deg)`;
+
+  document.getElementById('hand-minute').style.transform =
+    `translate(-50%, 0) rotate(${minutesDeg}deg)`;
+}
+
+
+tick();
+
+
+setInterval(tick, 100);
